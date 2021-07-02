8412578ffb734ad5f951aa820b2368e1e700d0a9
massive2351 <massive2351@gmail.com>
2021-07-02 03:01:41 +0000

    settings.pyのカスタマイズ

--- tutorial/settings.py
+++ tutorial/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'bootstrap4',    
+    'widget_tweaks',  
+    'pdfmr',  
 ]
 
 MIDDLEWARE = [
@@ -54,7 +57,7 @@ ROOT_URLCONF = 'tutorial.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR,'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
