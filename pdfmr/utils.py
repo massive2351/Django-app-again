3156a85282a7ad8b904c9c7c697f49ed839968ec
massive2351 <massive2351@gmail.com>
2021-07-06 06:25:11 +0000

    create excel メソッド

--- pdfmr/utils.py
+++ pdfmr/utils.py
@@ -4,6 +4,14 @@ from pdfminer.layout import LAParams
 from pdfminer.pdfpage import PDFPage
 from io import StringIO
 
+from glob import glob
+from django.conf import settings
+import os
+import shutil
+import openpyxl
+import random, string
+import time
+
 
 def convert_pdf_to_txt(path):
     """pdfからテキスト情報を抽出する関数"""
@@ -28,3 +36,26 @@ def convert_pdf_to_txt(path):
     device.close()
     retstr.close()
     return fstr
+    
+    
+def create_excel(upload_dir,user_name):
+    """Excelデータを生成する関数"""
+    #アップロードしたファイルの取り込み
+    upload_path = os.path.join(upload_dir, "*.pdf")    
+    template_file = os.path.join(settings.MEDIA_ROOT, "template","請求書一覧ファイル.xlsx")
+    timestr = time.strftime("%Y%m%d-%H%M%S")
+    work_file =os.path.join(settings.MEDIA_ROOT, "temp", "請求書一覧ファイル_" + timestr + ".xlsx")  
+    user_dir = os.path.join(settings.MEDIA_ROOT , "excel", user_name) 
+    file_list = glob(upload_path)   #uploadされたPDFファイルリストを取得
+    shutil.copyfile(template_file, work_file)  #テンプレートファイルをコピー
+    book = openpyxl.load_workbook(work_file)   #Excelファイルオープン
+
+    result_list =[]
+    for pdf in file_list:
+        """アップロードされたPDFファイルを1つずつ読み込んでText化する。"""
+        result_txt = convert_pdf_to_txt(pdf)
+        result_list.append(result_txt)
+
+    # merge_excel(book,result_list,work_file) #Excelにデータをセット           
+    #個人ディレクトリへコピー
+    shutil.move(work_file, user_dir)
