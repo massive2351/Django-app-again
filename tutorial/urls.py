dfb277fa7cbb7a668a07a46f53d75bcde1ce00d5
massive2351 <massive2351@gmail.com>
2021-07-10 11:28:05 +0000

    メディアファイルのURLパターンの定義

--- tutorial/urls.py
+++ tutorial/urls.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include   #変更
+from django.conf import settings #追加
+from django.conf.urls.static import static #追加
 
 urlpatterns = [
     path('admin/', admin.site.urls),
